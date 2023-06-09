7c36ceac9ad7c813cd7692839d8bd6981498acaf
Jayden Major <jaydenmajor@gmail.com>
2023-06-09 17:35:40 +1000

    Added Sitemap XML

--- quickbooks-resources.php
+++ quickbooks-resources.php
@@ -82,7 +82,54 @@ function bizink_quickbooks_init(){
 		add_rewrite_rule("^".$post->post_name."/([a-z0-9-]+)[/]?$",'index.php?pagename=quickbooks-resources&bizpress=$matches[1]','top');
 		add_rewrite_rule("^".$post->post_name."/topic/([a-z0-9-]+)[/]?$",'index.php?pagename=quickbooks-resources&topic=$matches[1]','top');
 		add_rewrite_rule("^".$post->post_name."/type/([a-z0-9-]+)[/]?$" ,'index.php?pagename=quickbooks-resources&type=$matches[1]','top');
-		//flush_rewrite_rules();
+		
+		add_rewrite_tag('%quickbooks_resources.xml%', '([^&]+)', 'bizpressxml=');
+		add_rewrite_rule('^(quickbooks_resources\.xml)?$','index.php?bizpressxml=quickbooks_resources','top');
+
+		if(get_option('bizpress_quickbooks_flush_update',0) < 1){
+			flush_rewrite_rules();
+			update_option('bizpress_quickbooks_flush_update',1);
+		}
+	}
+}
+
+add_action('parse_request','bizpress_quickbooksxml_request', 10, 1);
+function bizpress_quickbooksxml_request($wp){
+	if ( array_key_exists( 'bizpressxml', $wp->query_vars ) && $wp->query_vars['bizpressxml'] == 'quickbooks_resources'){
+		$post = bizink_get_quickbooks_page_object();
+		if( is_object( $post ) && get_post_type( $post ) == "page" ){
+			$data = get_transient("bizinktype_".md5('quickbooks-content'));
+			if(empty($data)){
+				$data = bizink_get_content('quickbooks-content', 'topics');
+				set_transient( "bizinktype_".md5('quickbooks-content'), $data, (DAY_IN_SECONDS * 2) );
+			}
+			header('Content-Type: text/xml; charset=UTF-8');
+			echo '<?xml version="1.0" encoding="UTF-8"?>';
+			$url = get_home_url();
+			$url = str_replace('https:','',$url);
+			$url = str_replace('http:','',$url);
+			echo '<?xml-stylesheet type="text/xsl" href="//'.$url.'/wp-content/plugins/wordpress-seo/css/main-sitemap.xsl"?>';
+			echo '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+			
+			echo '<url>';
+			echo '<loc>'.get_home_url().'/'.$post->post_name.'</loc>';
+			echo '</url>';
+			
+			if(empty($data->posts) == false){
+				foreach($data->posts as $item){
+					echo '<url>';
+					echo '<loc>'.get_home_url().'/'.$post->post_name.'/'. $item->slug .'</loc>';
+					if($item->thumbnail){
+						echo '<image:image>';
+						echo '<image:loc>'. $item->thumbnail .'</image:loc>';
+						echo '</image:image>'; 
+					}
+					echo '</url>';
+				}
+			}
+			echo '</urlset>';
+		}
+		die();
 	}
 }
 
@@ -91,3 +138,19 @@ function bizpress_quickbooks_qurey($vars) {
     $vars[] = "bizpress";
     return $vars;
 }
+
+add_filter('query_vars', 'bizpress_quickbooksxml_query');
+function bizpress_quickbooksxml_query($vars) {
+    $vars[] = "bizpressxml";
+    return $vars;
+}
+
+function bizpress_quickbooks_sitemap_custom_items( $sitemap_custom_items ) {
+    $sitemap_custom_items .= '
+	<sitemap>
+		<loc>'.get_home_url().'/quickbooks_resources.xml</loc>
+	</sitemap>';
+    return $sitemap_custom_items;
+}
+
+add_filter( 'wpseo_sitemap_index', 'bizpress_quickbooks_sitemap_custom_items' );
